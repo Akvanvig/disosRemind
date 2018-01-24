bfbfb0de3adc533936a77bfb1812c2e731481f9b
Anders Kvanvig <Abduhl Kasaii@DESKTOP-554JU4M>
2018-01-24 21:53:10 +0100

    Test

--- bot.js
+++ bot.js
@@ -9,29 +9,26 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 //initialize Discord bot
 var bot = new Discord.Client({
-  token: auth.token,
-  autorun: true
+    token: auth.token,
+    autorun: true
 })
 bot.on('ready', function (evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ')
-  logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Connected');
+    logger.info('Logged in as: ')
+    logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-  // Our bot needs to know if it will execute a command
-  // It will listen for messages that will start with '?'
-  if (message.substring(0, 1);
-    var args = message.substring(1).split(' ');
-    var cmd = args[0];
-    args = args.splice(1);
-    switch(cmd) {
-      //?ping
-      case 'ping'
-        bot.sendMessage({
-          to: channelID,
-          message: 'Pong?'
-        });
-      break;
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with '?'
+    if (message.substring(0, 1) == '?') { 
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        args = args.splice(1);
+        switch(cmd) {
+            //?ping
+            case 'ping'
+                bot.sendMessage({to: channelID, message: 'Pong?'});
+         break;
+        }
     }
-  }
 });
