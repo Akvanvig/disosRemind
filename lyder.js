0314d8423fd5939a8a7473c3aef7a75281e9d219
Anders Kvanvig <akvanv@gmail.com>
2018-02-28 01:10:22 +0100

    beskrivelse +InitialD

--- lyder.js
+++ lyder.js
@@ -150,6 +150,7 @@ module.exports = {
                 tekst += '\nKjeften';
                 tekst += '\nSkottland';
                 tekst += '\nInitialD';
+                tekst += '\n\t\tSpiller av tilfeldige InitialD sanger, kan flyttes til annen samtale ved å gjenta kommandoen.\n\t\tHvis du ønsker den skal forlate samtalen, bruk: +Leave'
                 bot.sendMessage({ to: channelID, message: tekst });
         }
     }
