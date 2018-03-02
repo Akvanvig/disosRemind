8a0172a738dda047b55298e385b7df71f5ee82ab
Anders Kvanvig <akvanv@gmail.com>
2018-03-02 09:25:43 +0100

    removes punctuation

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         }
         var nynorsk = '# Offentleg Samferdselsbodskap frå Språkrådet #'
         for (var i = 0; i < args.length; i++) {
-            switch (args[i].toLowerCase()) {
+            switch (args[i].toLowerCase().replace(/-,._'^*"()[]{}/g, '')) {
                 case 'korkje':
                     nynorsk += '\nkorkje = hverken';
                     break;
